4104e52ca67747a59991f4b7830b633f0d3d1131
Chuck Crandall <chuck@webchuckweb.com>
2019-01-07 17:28:07 -0700

    Adding config details and script updates.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,14 @@
-# run composer install
+#!/bin/bash
 
-# add ec2-user
+# run composer install
+composer install
+
+# add ec2-user to apache group (if needed)
+
+# copy localConfig
+if [ "$DEPLOYMENT_GROUP_NAME" == "UdoitProd" ]
+then
+    cp scripts/localConfig.prod.php scripts/localConfig.php
+else
+    cp scripts/localConfig.stage.php scripts/localConfig.php
+fi
