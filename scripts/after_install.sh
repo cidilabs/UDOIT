49db8e25804a6af8926e2061cf0d99fe2611f349
Chuck Crandall <chuck@webchuckweb.com>
2019-01-10 14:29:49 -0700

    AWS codedeploy config updates.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# change to the new file location
+cd /var/www/html
+
 # run composer install
 composer install
-
-# add ec2-user to apache group (if needed)
 
 # copy localConfig
 if [ "$DEPLOYMENT_GROUP_NAME" == "UdoitProd" ]
 then
-    cp scripts/localConfig.prod.php scripts/localConfig.php
+    cp /home/ec2-user/config/udoit/localConfig.prod.php scripts/localConfig.php
 else
-    cp scripts/localConfig.stage.php scripts/localConfig.php
+    cp /home/ec2-user/config/udoit/localConfig.stage.php scripts/localConfig.php
 fi
+
+# change localconfig owner
+chown ec2-user:apache scripts/localConfig.php
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,6 @@
 # change permissions to /var/www
+sudo chown -R apache:apache /var/www
+sudo chmod -R 775 /var/www
 
+# remove all files in /var/www/html
+sudo rm -rf /var/www/html/*
