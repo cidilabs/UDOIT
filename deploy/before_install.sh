9e74689c6fdd998b461218388ab794ab0bd4563e
Chuck Crandall <chuck@webchuckweb.com>
2019-01-16 16:02:50 -0700

    deploy change.

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # change permissions to /var/www
-sudo chown -R apache:apache /var/www
-sudo chmod -R 775 /var/www
+#chown -R apache:apache /var/www
+#chmod -R 775 /var/www
 
 # remove all files in /var/www/html
-sudo rm -rf /var/www/html/*
+rm -rf /var/www/html/*
