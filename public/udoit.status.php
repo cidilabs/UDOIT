7542513272c5c3f8d2c13510e80168c432094b4e
Chuck Crandall <chuck@webchuckweb.com>
2020-01-07 11:09:10 -0700

    Adding version to status check

--- public/udoit.status.php
+++ public/udoit.status.php
@@ -34,7 +34,7 @@ $sth->execute();
 $url = $sth->fetch();
 $canvas_url = $url[0];
 
-$status['tool'] = "UDOIT";
+$status['tool'] = "UDOIT " . UDOIT_VERSION;
 $status['url'] = $server_link;
 $status['base_url'] = $canvas_url;
 
