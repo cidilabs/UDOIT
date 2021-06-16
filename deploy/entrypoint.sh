0314918fdb57c2f145c0a7c209b687cc80393cf5
Scott Cooper <scott@cidilabs.com>
2021-06-16 15:22:48 -0600

    change order of enterypoint script ran

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -21,11 +21,11 @@ yarn run encore dev
 # start queue monitor
 /usr/bin/supervisord
 
-#change owner of all files.
-chown -R ssm-user:www-data /var/www/html
-
 # Start Nginx
 service nginx start
 
 #Start PHP-FPM
 php-fpm
+
+#change owner of all files.
+chown -R ssm-user:www-data /var/www/html
