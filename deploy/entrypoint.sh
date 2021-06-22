444de9db8ef5d5fb1b42547055743ec53b91c150
Scott Cooper <scott@cidilabs.com>
2021-06-22 15:48:13 -0600

    change installtion of new relic

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -10,9 +10,17 @@ fi
 if [ "$ENVIORNMENT_TYPE" != "local" ]
 then
     # create .user.ini file for New Relic (PHP-FPM only)
-    touch /var/www/html/public/.user.ini
-    # add New Relic appname 
-    echo -e "\nnewrelic.appname = \"$NEW_RELIC_APP_NAME\"" >> /var/www/html/public/.user.ini
+    curl -L newrelic-php5-9.17.1.301-linux.tar.gz | tar -C /tmp -zx && \
+    export NR_INSTALL_USE_CP_NOT_LN=1 && \
+    export NR_INSTALL_SILENT=1 && \
+    /tmp/newrelic-php5-*/newrelic-install install && \
+    rm -rf /tmp/newrelic-php5-* /tmp/nrinstall* && \
+    sed -i \
+      -e 's/"REPLACE_WITH_REAL_KEY"/"$NEW_RELIC_LICENSE_KEY"/' \
+      -e 's/newrelic.appname = "PHP Application"/newrelic.appname = "$NEW_RELIC_APP_NAME"/' \
+      -e 's/;newrelic.daemon.app_connect_timeout =.*/newrelic.daemon.app_connect_timeout=15s/' \
+      -e 's/;newrelic.daemon.start_timeout =.*/newrelic.daemon.start_timeout=5s/' \
+      /usr/local/etc/php/conf.d/newrelic.ini
 fi
 
 # compile JS
