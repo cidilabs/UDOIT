d46c8cd9da5018591fecc91909685903a9a54986
Scott Cooper <scott@cidilabs.com>
2021-06-28 09:23:24 -0600

    remove chown

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-find /var/www/html -type f -exec chmod 664 {} + -o -type d -exec chmod 775 {} +
-
 # copy localConfig from S3 if you are not on local
 if [ "$ENVIORNMENT_TYPE" != "local" ]
 then
     aws s3 cp s3://cidilabs-devops/udoit3/.env.local.$ENVIORNMENT_TYPE /var/www/html/.env.local
 fi
+
+find /var/www/html -type f -exec chmod 664 {} + -o -type d -exec chmod 775 {} +
 
 # compile JS
 yarn run encore dev
@@ -23,6 +23,5 @@ service nginx start
 #Start PHP-FPM
 php-fpm
 
-
 #change owner of all files.
-chown -R ssm-user:www-data /var/www/html
+#chown -R ssm-user:www-data /var/www/html
