3196c9994af4b2ff3f15f07d736ddd138861ec7c
Scott Cooper <scott@cidilabs.com>
2021-06-28 17:32:45 -0600

    move clear cache

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd /var/www/html
 
-# copy localConfig from S3 if you are not on local
+# copy localConfig from S3 if you are not on local and clear cache
 if [ "$ENVIORNMENT_TYPE" != "local" ]
 then
     aws s3 cp s3://cidilabs-devops/udoit3/.env.local.$ENVIORNMENT_TYPE /var/www/html/.env.local
+    php bin/console cache:clear --env=$ENVIORNMENT_TYPE
 fi
 
 # compile JS
@@ -21,9 +22,3 @@ service nginx start
 
 #Start PHP-FPM
 php-fpm
-
-# Run Cache clear on prod
-if [ "$ENVIORNMENT_TYPE" == "prod" ]
-then
-    php bin/console cache:clear --env=prod
-fi
