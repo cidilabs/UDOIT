3036e161d18f6f36051ae5ab256b1e2f03417a16
Scott Cooper <scott@cidilabs.com>
2021-06-28 17:02:30 -0600

    add clear cache for prod

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,6 +7,12 @@ then
     aws s3 cp s3://cidilabs-devops/udoit3/.env.local.$ENVIORNMENT_TYPE /var/www/html/.env.local
 fi
 
+# copy localConfig from S3 if you are not on local
+if [ "$ENVIORNMENT_TYPE" == "prod" ]
+then
+    php bin/console cache:clear --env=prod
+fi
+
 # compile JS
 yarn run encore dev
 
