e8fe268fda1ccd6ffdc6bc2ea933f029fe090a6e
Ethan  Finlay <ethanfinlay@MacBook-Pro.attlocal.net>
2021-09-13 16:02:42 -0400

    ifixing blank screen bug with color names and improving on rgb2hex

--- assets/js/Services/Contrast.js
+++ assets/js/Services/Contrast.js
@@ -225,6 +225,12 @@ class Contrast {
     return '#' + color
   }
 
+  standardizeColor(str){
+    var ctx = document.createElement("canvas").getContext("2d");
+    ctx.fillStyle = str;
+    return ctx.fillStyle;
+  }
+
 }
 
 export default new Contrast()
